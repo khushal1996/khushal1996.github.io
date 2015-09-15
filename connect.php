b487460ef0a84af6bf64f150af170ec921e1dc27
khushal1996 <f2014094@goa.bits-pilani.ac.in>
2015-09-15 10:47:08 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 
 
- $connect = mysql_connect("Idontexist","root","") or die("conection failed");
+ $connect = mysql_connect("192.168.137.1","root","") or die("conection failed");
  echo "Connected!";
 
 
